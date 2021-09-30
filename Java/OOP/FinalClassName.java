4fe4ac131fc126815c0a1fd78e0fa4936b5dc6d0
rezwan2525 <rezwan2525@gmail.com>
2021-10-01 01:36:12 +0600

    Final class

---
+++ Java/OOP/FinalClassName.java
@@ -0,0 +1,12 @@
+package OOP;
+
+
+//just put final keyword in front of class keyword ===> That class can't be inherited
+public final class FinalClassName {
+
+    private int someVar ;
+
+    public FinalClassName(int someVar){
+        this.someVar = someVar;
+    }
+}
